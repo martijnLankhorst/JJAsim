3e0190e28b974a6eb88cff1b473a42e7bfe654f0
martijnLankhorst <54381378+martijnLankhorst@users.noreply.github.com>
2019-08-29 22:07:40 +0200

    Add files via upload

--- 2D/network/methods/JJAsim_2D_network_method_getU.m
+++ 2D/network/methods/JJAsim_2D_network_method_getU.m
@@ -2,14 +2,14 @@ function U = JJAsim_2D_network_method_getU(array,V)
 %U = JJAsim_2D_network_method_getU(array,V)
 %
 %DESCRIPTION
-% obtains the island potential U from the junction voltage drop V. 
+% obtains the node potential U from the junction voltage drop V. 
 %
 %FIXED INPUT
 % array     struct                  information about Josephson junction array    
 % V         Nj* by W by Nt          junction voltage drop
 %
 %OUTPUT
-% U         Np by W by Nt           island potential
+% U         Nn by W by Nt           node potential
 
 if size(V,1) == 1
     V = repmat(V,array.Nj,1);
@@ -17,11 +17,11 @@ end
 Cred = array.CComponentsReduced;
 Ncomp = array.nrOfConnectedComponents;
 if Ncomp > 1
-    iscomp = array.islandComponents;
+    iscomp = array.nodeComponents;
 else
-    iscomp = ones(array.Nis,1);
+    iscomp = ones(array.Nn,1);
 end
 sz = size(V);
 U = -JJAsim_2D_network_method_CSolve(array.M*reshape(V,array.Nj,[]),Cred,iscomp,Ncomp);
-U = reshape(U,[array.Nis,sz(2:end)]);
+U = reshape(U,[array.Nn,sz(2:end)]);
 end
--- 2D/network/methods/JJAsim_2D_network_method_getphi.m
+++ 2D/network/methods/JJAsim_2D_network_method_getphi.m
@@ -2,7 +2,7 @@ function phi = JJAsim_2D_network_method_getphi(array,th)
 %phi = JJAsim_2D_network_method_getphi(array,th)
 %
 %DESCRIPTION
-% obtains the gauge dependent phase at each island from the gauge invariant 
+% obtains the gauge dependent phase at each node from the gauge invariant 
 % phase difference th.
 %
 %FIXED INPUT
@@ -10,15 +10,15 @@ function phi = JJAsim_2D_network_method_getphi(array,th)
 % th        Nj* by W by Nt       gauge invariant phase difference
 %
 %OUTPUT
-% phi       Np by W by Nt        gauge dependent phase at each island
+% phi       Nn by W by Nt        gauge dependent phase at each node
 
 if size(th,1) == 1
     th = repmat(th,array.Nj,1);
 end
 Cred = array.CComponentsReduced;
 Ncomp = array.nrOfConnectedComponents;
-iscomp = array.islandComponents;
+iscomp = array.nodeComponents;
 sz = size(th);
 phi = -JJAsim_2D_network_method_CSolve(array.M*reshape(th,array.Nj,[]),Cred,iscomp,Ncomp);
-phi = reshape(phi,[array.Nis,sz(2:end)]);
+phi = reshape(phi,[array.Nn,sz(2:end)]);
 end
